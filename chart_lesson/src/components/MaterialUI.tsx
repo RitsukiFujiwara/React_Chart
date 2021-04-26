6870ddcc7e110f382ba7aa3fc3b84c00e1b97b3b
Fujiwara <r9.20important@icloud.com>
2021-04-26 18:47:54 +0900

    Typographyまで終了

---
+++ chart_lesson/src/components/MaterialUI.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Button,makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    btnStyle:{
+        background: "green",
+        padding: "3px 50px",
+    },
+    typoStyle:{
+        color: "blue",
+    }
+})
+
+const MaterialUI : React.FC = () => {
+    const classes = useStyles();
+    return (
+        <div>
+        <Typography className={classes.typoStyle} color="secondary" variant="h1" align="left" gutterBottom>
+            Hello UI
+        </Typography>
+            <Button className={classes.btnStyle} variant="contained" color="primary">Test Button</Button>
+        </div>
+    )
+}
+
+export default MaterialUI
